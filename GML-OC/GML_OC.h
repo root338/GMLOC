6c6a7210818f0734a467d5e7157e2a804a99de5f
CG <peichenguang@changjing.ai>
2022-05-23 11:23:39 +0800

    Initial Commit

---
+++ GML-OC/GML_OC.h
@@ -0,0 +1,18 @@
+//
+//  GML_OC.h
+//  GML-OC
+//
+//  Created by GML on 2022/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GML_OC.
+FOUNDATION_EXPORT double GML_OCVersionNumber;
+
+//! Project version string for GML_OC.
+FOUNDATION_EXPORT const unsigned char GML_OCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GML_OC/PublicHeader.h>
+
+
